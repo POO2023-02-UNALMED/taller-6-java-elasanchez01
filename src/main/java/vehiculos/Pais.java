0c683daa3d4dc7659b25ffb1796df7e738cf0b63
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-03 11:29:57 -0500

    Update Pais.java

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -3,6 +3,31 @@ package vehiculos;
 import java.util.List;
 import java.util.ArrayList;
 
-public Class País {
+public class Pais {
   private String nombre;
+  private int cantidadVendidos=0;
+
+  private static ArrayList<Pais> listaPaises= new ArrayList<Pais>(); 
+
+  public Pais(String nombre){
+    this.nombre=nombre;
+    Pais.listaPaises.add(this);
+    }
+
+  public void setNombre(String nombre) {
+    this.nombre=nombre;
+  }
+  
+	public String getNombre() {
+    return this.nombre;
+  }
+	
+	public void setCantidadVendidos(int num) {
+    this.cantidadVendidos=num;
+  }
+  
+	public int getCantidadVendidos() {
+    return this.cantidadVendidos;
+  }
+
 }
