a49f74ee2fc3be0b47f1ede8909598afe90dca24
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-03 11:21:46 -0500

    Update Fabricante.java
    aaaaah

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class Fabricante {
   private String nombre;
   private Pais pais;
+  private int cantidadFrabricados=0;
+
+  public Fabricante(String nombre, Pais pais){
+    this.nombre = nombre;
+    this,pais = pais;
+  }
+
+  public void fabricar() {
+    this.cantidadFabricados++;
+    this.pais.setCantidadVendidos(this.pais.getCantidadVendidos()+1);
+  }
 }
