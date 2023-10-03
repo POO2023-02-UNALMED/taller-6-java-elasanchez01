85d0fd098a58580d9a310f7dce56dab6bccf1dfb
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-03 11:03:02 -0500

    Update Vehiculo.java

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -10,4 +10,17 @@ public class Vehiculo {
   private String traccion;
   private Fabricante fabricante;
   private static int CantidadVehiculos = 0;
+
+  public Vehiculo(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso, String traccion, Fabricante fabricante){
+    this.placa = placa;
+		this.puertas = puertas;
+		this.velocidadMaxima = velocidadMaxima;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.peso = peso;
+		this.traccion = traccion;
+		this.fabricante = fabricante;
+		**Fabricante.incrementarContador(fabricante);
+		cantidadVehiculos++;
+  }
 }
