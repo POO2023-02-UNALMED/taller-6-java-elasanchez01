eef1414b4434f8357748f639cfb82f9db686adfa
elasanchez01 <110686219+elasanchez01@users.noreply.github.com>
2023-10-03 10:49:30 -0500

    Update Vehiculo.java

--- src/main/java/vehiculos/Vehiculo.java
+++ src/main/java/vehiculos/Vehiculo.java
@@ -1,1 +1,13 @@
+package vehiculos;
 
+public class Vehiculo {
+  private String placa;
+  private int puertas;
+  private int velocidadMaxima;
+  private String nombre;
+  private int precio;
+  private int peso;
+  private String traccion;
+  private Fabricante fabricante;
+  private static int CantidadVehiculos = 0;
+}
